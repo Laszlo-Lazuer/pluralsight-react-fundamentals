7020afec6d94b7276d30f97e2702bd3fc835b760
Laszlo-Lazuer <visualjazz@gmail.com>
2018-08-02 13:48:13 -0500

    Add components directory & add a new component Hello with time

---
+++ src/Components/HelloTime.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+function Hello(props) {
+    return <h1>Hello at {props.now}</h1>;
+}
+
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './Components/HelloTime';
 import registerServiceWorker from './registerServiceWorker';
 
 let model = { clicks: 0 };
 
 function render() {
     ReactDOM.render(
+        <div>
+        <h2>Div Click count</h2>            
         <App
             clicks={model.clicks}
             onClick={() => { model.clicks +=1; render();}}
-        />,
+        />
+        <h2>Hello Component w/ Time</h2>
+        <Hello now={new Date().toISOString()} />
+        </div>,
         document.getElementById('root'));
 }
 
